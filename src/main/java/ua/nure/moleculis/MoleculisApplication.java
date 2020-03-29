d14d5d5222afc73a0cf5c8e738365acdddd08718
Eugene <ostashko4@gmail.com>
2020-03-29 17:06:01 +0300

    Initial commit
    Signed-off-by: Eugene <ostashko4@gmail.com>

---
+++ src/main/java/ua/nure/moleculis/MoleculisApplication.java
@@ -0,0 +1,11 @@
+package ua.nure.moleculis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoleculisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MoleculisApplication.class, args);
+    }
+}
