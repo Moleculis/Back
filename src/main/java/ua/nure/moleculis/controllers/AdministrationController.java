f750b5b162119876dfed748d87e86f7dc2acfadf
Eugene Ostashko <ostashko4@gmail.com>
2020-08-26 22:31:12 +0300

    Add get db backups and restore db backup

--- src/main/java/ua/nure/moleculis/controllers/AdministrationController.java
+++ src/main/java/ua/nure/moleculis/controllers/AdministrationController.java
@@ -4,24 +4,42 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import ua.nure.moleculis.models.dto.ListDTO;
 import ua.nure.moleculis.models.dto.MessageDTO;
 import ua.nure.moleculis.services.AdministrationService;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @RestController
 @RequestMapping("/administration")
 public class AdministrationController {
+    private final String backupsEndpoint = "/backups";
+
     private final AdministrationService administrationService;
 
     public AdministrationController(AdministrationService administrationService) {
         this.administrationService = administrationService;
     }
 
-    @PostMapping("/backup")
+    @PostMapping(backupsEndpoint)
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<MessageDTO> createDbBackup(HttpServletRequest request) {
         final String message = administrationService.createDatabaseBackup(request);
+        return new ResponseEntity<>(new MessageDTO(message), HttpStatus.OK);
+    }
+
+    @GetMapping(backupsEndpoint)
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<ListDTO<String>> getBackups(HttpServletRequest request) {
+        final List<String> backups = administrationService.getBackupFileNames(request);
+        return new ResponseEntity<>(new ListDTO<>(backups), HttpStatus.OK);
+    }
+
+    @PutMapping(backupsEndpoint)
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<MessageDTO> restoreBackup(@RequestParam String backup, HttpServletRequest request) {
+        final String message = administrationService.restoreDatabaseFromBackup(backup, request);
         return new ResponseEntity<>(new MessageDTO(message), HttpStatus.OK);
     }
 }
--- src/main/java/ua/nure/moleculis/services/AdministrationService.java
+++ src/main/java/ua/nure/moleculis/services/AdministrationService.java
@@ -1,25 +1,26 @@
 package ua.nure.moleculis.services;
 
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import ua.nure.moleculis.components.Translator;
 import ua.nure.moleculis.exception.CustomException;
 import ua.nure.moleculis.models.entitys.User;
 import ua.nure.moleculis.models.enums.Role;
-import ua.nure.moleculis.repos.UserRepo;
-import ua.nure.moleculis.security.JwtTokenProvider;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class AdministrationService {
+    final String backupFilesPath = "backups/";
+    final String dbHost = "localhost";
+    final String dbPort = "5432";
+    final String dbUser = "eugene";
+    final String dbName = "moleculisDB";
 
     private final UserService userService;
 
@@ -28,20 +29,14 @@ public class AdministrationService {
     }
 
     public String createDatabaseBackup(HttpServletRequest request) {
-        final User currentUser = userService.currentUser(request);
-        if (!currentUser.getRoles().contains(Role.ROLE_ADMIN)) {
-            throw new CustomException(Translator.toLocale("notAdmin"), HttpStatus.UNAUTHORIZED);
-        }
-
-        final String backupFilesPath = "backups/";
-
+        checkIfUserIsAdmin(request);
         final String date = new SimpleDateFormat("dd-MM-yyyy-HH-mm-ss").format(new Date());
-        final String backupFileName = backupFilesPath + "backup_moleculisdb_" + date + ".bak";
+        final String backupFileName = backupFilesPath + "backup_" + dbName.toLowerCase() + "_" + date + ".bak";
         try {
-            final String createBackupCommand = "pg_dump moleculisDB -h localhost " +
-                    "-p 5432 -U eugene -w -c -f " + backupFileName;
+            final String[] createBackupCommand = new String[]{"pg_dump", dbName, "-h",
+                    dbHost, "-p", dbPort, "-U", dbUser, "-w", "-c", "-f", backupFileName};
             final ProcessBuilder pb =
-                    new ProcessBuilder(createBackupCommand.split(" "));
+                    new ProcessBuilder(createBackupCommand);
 
             final Process process = pb.start();
             process.waitFor();
@@ -49,6 +44,54 @@ public class AdministrationService {
         } catch (Exception e) {
             e.printStackTrace();
             throw new CustomException(Translator.toLocale("backupCreateError"), HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+    }
+
+    public String restoreDatabaseFromBackup(String backup, HttpServletRequest request) {
+        checkIfUserIsAdmin(request);
+        if(!getFileBackups().contains(backup)){
+            throw new CustomException(Translator.toLocale("noBackup"), HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+        final String backupName = backupFilesPath + backup;
+        final String[] restoreCommand = new String[]{"psql", "-U", dbUser,
+                "-f", backupName, "-h", dbHost, "-p", dbPort, dbName};
+        try {
+            final ProcessBuilder pb =
+                    new ProcessBuilder(restoreCommand);
+
+            final Process process = pb.start();
+            process.waitFor();
+            return Translator.toLocale("backupRestored");
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new CustomException(Translator.toLocale("backupRestoreFail"), HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+    }
+
+    public List<String> getBackupFileNames(HttpServletRequest request) {
+        checkIfUserIsAdmin(request);
+        return getFileBackups();
+    }
+
+    private List<String> getFileBackups(){
+        final List<String> backupFileNames = new ArrayList<>();
+        final File path = new File(backupFilesPath);
+
+        final File[] files = path.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    backupFileNames.add(file.getName());
+                }
+            }
+        }
+        return backupFileNames;
+    }
+
+    private void checkIfUserIsAdmin(HttpServletRequest request) {
+        final User currentUser = userService.currentUser(request);
+        if (!currentUser.getRoles().contains(Role.ROLE_ADMIN)) {
+            throw new CustomException(Translator.toLocale("notAdmin"), HttpStatus.UNAUTHORIZED);
         }
     }
 }
